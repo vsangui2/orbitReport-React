583d28ea743e2d1e1e52c8c0cf641d62dd8fdb81
Vincent Sanguinetti <vincent.m.sanguinetti@gmail.com>
2024-03-18 21:13:12 -0500

    GA#6 complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import "./styling.css";
+
+function Banner() {
+    return(
+        <div>
+            <header>
+                <h1>Orbit Report</h1>
+                </header>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    );
+};
+
+export default Banner;
